5e70886d326374889134c7f23f693b2d426a89e0
Daniel Riverol <34141421+DanielRiverol@users.noreply.github.com>
2024-09-19 11:18:11 -0300

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,12 @@
+# 1: Horarios de clases Escribir un programa que muestre el horario de clases de los días lunes a viernes, usando la instrucción print() y organizando las materias por días.
+
+
+
+
+# 2: Calculadora de ahorros Escribir un programa que guarde en variables la cantidad ahorrada en cada uno de los últimos tres meses y luego calcule el total de ahorros. Mostrar el resultado en pantalla.
+
+
+
+# 3: Cálculo del promedio de edad Guardar en variables la edad de cinco personas, calcular el promedio de esas edades y mostrar el resultado en pantalla.
+
+
